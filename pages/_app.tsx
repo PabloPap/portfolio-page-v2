ec7c4d277720dd242ac5dd2f763f6cfef82de124
Pavlos Papadopoulos <pavlospapadopou@gmail.com>
2020-11-16 22:31:06 +0200

    added sass pkg and 7.1 pattern

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss'
+import '../site.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,22 @@ const Home = () => {
   return (
     <>
       <Header />
-      <h1>my home page</h1>
+      <div className="home">
+        <h1>my home page</h1>
+        <div className="home__cards">
+          <div className="home__cards--card">
+            <a href="#">link 1</a>
+          </div>
+          <div className="home__cards--card">
+            <a href="#">link 2</a>
+          </div>
+          <div className="home__cards--card">
+            <a href="#">link3</a>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
