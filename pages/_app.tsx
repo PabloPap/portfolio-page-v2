6a256187f05c244dcfb42d010d89353bebb8a63f
pablo <pavlospapadopou@gmail.com>
2022-05-07 16:38:16 +0300

    error handling test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,28 +4,31 @@ import ScrollToTop from 'react-scroll-up';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import '../site.scss';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="container">
-      <Header />
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-      <Footer />
-      <ScrollToTop
-        showUnder={800}
-        duration={500}
-        style={{
-          bottom: '180px',
-          right: '30px',
-          borderRadius: '50%',
-          backgroundColor: '#fff',
-          padding: '10px',
-          boxShadow: '1px 1px 10px 2px rgba(0, 0, 0, 0.1)',
-        }}
-      >
-        <span className="scrollToTop"></span>
-      </ScrollToTop>
+      <ErrorBoundary>
+        <Header />
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+        <Footer />
+        <ScrollToTop
+          showUnder={800}
+          duration={500}
+          style={{
+            bottom: '180px',
+            right: '30px',
+            borderRadius: '50%',
+            backgroundColor: '#fff',
+            padding: '10px',
+            boxShadow: '1px 1px 10px 2px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <span className="scrollToTop"></span>
+        </ScrollToTop>
+      </ErrorBoundary>
     </div>
   );
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const { BLOG_URL, CONTENT_API_KEY } = process.env;
 
@@ -12,8 +13,17 @@ type Post = {
 
 async function getPosts() {
   const res = await fetch(
-    `${BLOG_URL}/ghost/api/v4/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,published_at`,
-  ).then((res) => res.json());
+    `${BLOG_URL}/ghost/api/v4/content/possts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,published_at`,
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Could not fetch data');
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 
   const posts = res.posts;
   return posts;
@@ -38,6 +48,8 @@ const formatDate = (date: string) => {
 
 const Blog: React.FC<{ posts: Post[] }> = (props) => {
   const { posts } = props;
+
+  // useEffect(() => {});
 
   return (
     <>
